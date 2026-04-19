6e2ba6ccecf936313c9b1ae75f93875e37dc1da4
Arianna <arili5.cas@gmail.com>
2026-04-19 17:24:56 +0200

    q paso aqui

--- Documents/NetBeansProjects/DAM/src/dam/DAM.java
+++ Documents/NetBeansProjects/DAM/src/dam/DAM.java
@@ -15,6 +15,8 @@ public class DAM {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("primeros pasos");
     }
     
 }
